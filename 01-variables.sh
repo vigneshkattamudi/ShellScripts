4f98110c526b65245fd76db386dd7f970d803b52
kvignesh0502 <vkattamudi88@gmail.com>
2023-12-13 19:19:35 +0000

    changed

--- 01-variables.sh
+++ 01-variables.sh
@@ -6,4 +6,4 @@ PERSON2=Raheem
 echo " $PERSON1: Hello $PERSON2, Good Morning"
 echo " $PERSON2: Hi $PERSON1, very Good Morning"
 echo " $PERSON1: How are you $PERSON2?"
-echo " $PERSON2: I am good $PERSON1, How are you"
+echo " $PERSON2: I am good $PERSON1, How are you "
