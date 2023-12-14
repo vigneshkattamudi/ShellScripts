43ad3189348e70bf469bc132a6ddd76284df0cfe
kvignesh0502 <vkattamudi88@gmail.com>
2023-12-14 19:31:53 +0000

    modified

---
+++ 12.logs.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+ID=$(id -u)
+echo " Script name: $0 " 
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo " Error: $1  is failed "
+        exit 1
+    else 
+        echo " $2  Success "
+    fi
+
+}
+
+if [ $ID -ne 0 ]
+then
+    echo " ERROR : login with root user to run the script "
+    exit 1
+else
+    echo " you are root user "
+fi
+
+yum install mysql -y
+
+VALIDATE $? " MySQL installed"
+
+yum install git -y
+
+VALIDATE $? " Git Installed "
