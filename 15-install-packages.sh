162d0072255d416b2babaee0a8d0a17116c7308f
kvignesh0502 <vkattamudi88@gmail.com>
2023-12-17 23:15:22 +0000

    Added new script

--- 15-install-packages.sh
+++ 15-install-packages.sh
@@ -2,9 +2,14 @@
 
 ID=$(id -u)
 
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+
 if [ $ID -ne 0 ]
 then
-    echo "ERROR : login with root user to run the script "
+    echo "ERROR : login with $R root user $N to run the script "
     exit 1 #we can give other than zero
 else
     echo -e "You are $G Root user $N "
